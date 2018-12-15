9565dfa4720e8725cfff1cb0289e837465843e76
Myles Enriquez <enri.myles@gmail.com>
2018-12-14 16:05:02 -0800

    Update.

--- day01/part2/ChronalCalibration.java
+++ day01/part2/ChronalCalibration.java
@@ -12,30 +12,24 @@ public class ChronalCalibration {
 
     String str;
     int total = 0;
-    int storedFrequency = 0;
 
     // Create blank hashmap
     HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
 
     // Read through each line
     while ( (str = br.readLine()) != null ) {
-      // for each token in file, search to see if hash map contains key
-      if (hmap.containsKey(Integer.parseInt(str))) {
-        System.out.println("Contains " + Integer.toString(intToSearchFor));
+      total = total + Integer.parseInt(str);
+      
+      if (hmap.containsKey(total)) {
+        hmap.put(total, hmap.get(total) + 1);
+        if (hmap.get(total) == 2) {
+          System.out.println("Reocurring Frequency: " + Integer.toString(total));
+          break;
+        } 
+      } else {
+        hmap.put(total, 1);
       }
-      // if contains key, check key's value
-        // if value is 2, return key
-      // else add kay to hashmap
     }
-    
-    
-    System.out.println("Before:" + hmap);
-    hmap.put(1, 0);
-    System.out.println("After:" + hmap);
-    int intToSearchFor = 1;
-    if (hmap.containsKey(intToSearchFor)) {
-      System.out.println("Contains " + Integer.toString(intToSearchFor));
-    }
-
+    br.close();
   }
 }
